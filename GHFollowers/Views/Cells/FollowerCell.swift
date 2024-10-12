a1b81db4ef1b7c8e51f79fce0d5b564c84abec65
Yasin Akça <yasin.akca@vakifbank.com.tr>
2024-10-12 18:36:32 +0300

    image download function added

--- GHFollowers/Views/Cells/FollowerCell.swift
+++ GHFollowers/Views/Cells/FollowerCell.swift
@@ -25,6 +25,7 @@ final class FollowerCell: UICollectionViewCell {
     
     func populate(for follower: Follower) {
         usernameLabel.text = follower.login
+        avatarImageView.downloadImage(from: follower.avatarUrl ?? "")
     }
     
     private func configure() {
--- GHFollowers/Views/Cells/GFAvatarImageView.swift
+++ GHFollowers/Views/Cells/GFAvatarImageView.swift
@@ -26,4 +26,16 @@ final class GFAvatarImageView: UIImageView {
         layer.cornerRadius = 10
         image = placeholderImage
     }
+    
+    func downloadImage(from urlString: String) {
+        guard let url = URL(string: urlString) else { return }
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
+            if error != nil { return }
+            guard let data = data, let image = UIImage(data: data), let self = self else { return }
+            DispatchQueue.main.async {
+                self.image = image
+            }
+        }
+        task.resume()
+    }
 }
